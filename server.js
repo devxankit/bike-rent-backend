48c192f29d36877d6bf1dba3f333855b108873dc
Ankit <devxankit@gmail.com>
2025-07-12 17:02:40 +0530

    cors fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 next();
 });
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
